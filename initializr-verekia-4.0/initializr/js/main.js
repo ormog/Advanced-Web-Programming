1a2d8b75f9df856f8f2023e166cf76e06e085df3
Erik Van Maren <ormog@hotmail.com>
2013-02-02 08:29:10 -0500

    Final commit

--- initializr-verekia-4.0/initializr/js/main.js
+++ initializr-verekia-4.0/initializr/js/main.js
@@ -1,16 +1,3 @@
 //File Name: main.js
 //Author Name: Erik Van Maren
 //Description: 
-
-
-
-
-
-function buttonClick(){
-	alert("poke");
-}
-
-
-window.onload = function(){
-	document.getElementById("myButton").onclick = buttonClick;
-}
